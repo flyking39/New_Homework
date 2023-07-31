28a45735ba6ef4082680f5c9bdf1badea0fef3d0
Philippp <lotsmanov.filipp@gmail.com>
2023-07-31 08:47:44 -0700

    added the first homework

--- Homework.py
+++ Homework.py
@@ -1,15 +1,30 @@
 # Нужно найти, какие два числа из списков в результате попарного перемножения дадут число 56.
+from typing import Any
 list_1 = [2, 5, 7, 10]
 list_2 = [3, 8, 4, 9]
 to_find = 56
-can_continue = True
-for x in list_1:
-    for y in list_2:
-        result = x * y
-        print(x, y, result)
-        if result == to_find:
-            print('Found!!!')
-            can_continue = False
-            break
-    if not can_continue:
-        break
+
+
+def multiplication(list_1: list, list_2: list, to_find: int) -> Any:
+    """
+    description
+    :param list_1:
+    :param list_2:
+    :param to_find:
+    :return:
+    """
+    for x in list_1:
+        for y in list_2:
+            result = x * y
+            yield result
+            print(x, y, result)
+            if result == to_find:
+                print('found')
+                return result
+
+
+multiplication(list_1, list_2, to_find)
+
+item = multiplication(list_1, list_2, to_find)
+for x in item:
+    print(x)
